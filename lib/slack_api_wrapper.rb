c410b53fe23147b5247220843736bf6d73034562
Kaida Masaki <kaida@adadevelopersacademy.org>
2020-03-11 11:36:56 -0700

    Add solution from lecture

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -4,7 +4,25 @@ require "httparty"
 module SlackApiWrapper
   BASE_URL = "https://slack.com/api/"
   API_KEY = ENV["SLACK_TOKEN"]
-
+  POST_URL = "#{BASE_URL}/chat.postMessage"
+  
   # Code goes here
-
+  def self.send_msg(message, channel_name)
+    resp = HTTParty.post(POST_URL, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: {
+        token: API_KEY,
+        channel: channel_name,
+        text: message
+      }
+    })
+    
+    p POST_URL
+    p API_KEY
+    puts resp.body
+    
+    return resp.code == 200 && resp.parsed_response["ok"]
+  end
 end
--- test/slack_api_wrapper_test.rb
+++ test/slack_api_wrapper_test.rb
@@ -2,5 +2,10 @@
 require_relative 'test_helper'
 
 describe SlackApiWrapper do
-
+  it "can send a valid message" do
+    VCR.use_cassette("slack-posts") do
+      response = SlackApiWrapper.send_msg("Hey I can post messages!", "general")
+      expect(response).must_equal true
+    end
+  end
 end
